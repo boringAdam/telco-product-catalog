d113c53519dbd3ba2c8c4c605ce18ee509bc3863
Adam <mikeadam19999@gmail.com>
2025-11-14 05:19:53 +0100

    Initial commit

---
+++ src/main/java/com/example/telco/productcatalog/TelcoProductCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.telco.productcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelcoProductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelcoProductCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/telco/productcatalog/TelcoProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telco.productcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelcoProductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
